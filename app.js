6c2ff5b097a121d9fc9f3ad3bcfa0d335437653e
tlhurd <tigerlillyhurd@gmail.com>
2020-11-18 14:22:46 -0500

    updated store with quiz content

--- app.js
+++ app.js
@@ -6,24 +6,54 @@ const store = {
   // 5 or more questions are required
   questions: [
     {
-      question: 'What color is broccoli?',
+      question: 'Who is the Goddess-Queen of the Underworld?',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+        'Nyx',
+        'Persephone',
+        'Rhea',
+        'Tartarus'
       ],
-      correctAnswer: 'green'
+      correctAnswer: 'Persephone'
     },
     {
-      question: 'What is the current year?',
+      question: 'What does Ariadne give to Theseus in his quest to defeat the Minotaur of Crete?',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'a mirror shield',
+        'a lyre',
+        'a ball of thread',
+        'a ring'
       ],
-      correctAnswer: '2019'
+      correctAnswer: 'a ball of thread'
+    },
+    {
+      question: 'What is Dionysus the god of?',
+      answers: [
+        'madness; ecstasy; and wine',
+        'war; famine; and death',
+        'spring; life; and harmony',
+        'music; travel; and light'
+      ],
+      correctAnswer: 'madness; ecstasy; and wine'
+    },
+    {
+      question: 'What are the trials of Psyche?',
+      answers: [
+        'slaying the Nemean lion; clean the Augean stables in a single day; capture the Cretan Bull; Steal the Mares of Diomedes',
+        'capture the one eye of the three Graeae, retrieve the head of Medusa, rescue Andromeda',
+        'drown out the Siren\'s song with music; retrieve Eurydice from the underworld',
+        'sorting out a large pile of seeds in a single night; retrieving the Golden Fleece; filling a flask of water from the River Styx; returning from the Underworld with a box of beauty ointment belonging to Proserpina'
+      ],
+      correctAnswer: 'sorting out a large pile of seeds in a single night; retrieving the Golden Fleece; filling a flask of water from the River Styx; returning from the Underworld with a box of beauty ointment belonging to Proserpina'
+    },
+    {
+      question: 'Odysseus is returning to his home of:',
+      answers: [
+        'Thrinacia',
+        'Ithica',
+        'Ogygia',
+        'Aeaea'
+      ],
+      correctAnswer: 'Ithica'
     }
   ],
   quizStarted: false,
@@ -49,6 +79,8 @@ const store = {
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
 // These functions return HTML templates
+
+
 
 /********** RENDER FUNCTION(S) **********/
 
